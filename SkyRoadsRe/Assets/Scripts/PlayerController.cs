bbdc321a947b9fb8532c791fba3cfc88b6c84500
Hrishik Arora <hrishik.arora@gmail.com>
2022-07-31 19:13:20 +0530

    change in player controller

--- SkyRoadsRe/Assets/Scripts/PlayerController.cs
+++ SkyRoadsRe/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     Vector2 move;
     Vector3 movement;
     bool isMovementPressed;
+    [SerializeField]float movementSpeed;
+    float acceleration;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -22,14 +24,63 @@ public class PlayerController : MonoBehaviour
     }
 
     
-    void Update()
+    void FixedUpdate()
+    {
+        HandleGravity();
+        HandleMovement();
+
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 
+    /// <summary>
+    /// This function handle gravity of the rocket
+    /// </summary>
+    /// 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void HandleGravity()
+    {
+        if(Physics.Raycast(transform.position,Vector3.down,out RaycastHit hit ,20.0f))
+        {
+            Debug.Log("jkdhf");
+            playerRb.useGravity = false;
+            
+        }
+        else
+        {
+            playerRb.useGravity = true;
+            playerRb.AddRelativeTorque(1f,0,0f);
+        }
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 
+    /// <summary>
+    /// This function handle movement of the rocket
+    /// </summary>
+    /// 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void HandleMovement()
     {
         if(isMovementPressed)
         {
-            Debug.Log("Working");
+            
+            Debug.Log(movement);
+            playerRb.velocity = new Vector3(-movement.x,movement.y,0) * Time.deltaTime * movementSpeed;
         }
+        else playerRb.velocity = new Vector3(0,playerRb.velocity.y,0);
     }
-    
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 
+    /// <summary>
+    /// This function records input from the user's device
+    /// </summary>
+    /// 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void OnMovementPressed(InputAction.CallbackContext ctx)
     {
         move = ctx.ReadValue<Vector2>();
