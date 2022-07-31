6489058282150604ef105ac0ad8991b2ca31850c
Hrishik Arora <hrishik.arora@gmail.com>
2022-07-31 16:40:42 +0530

    Created Player Movement Script

--- SkyRoadsRe/Assets/Scripts/PlayerController.cs
+++ SkyRoadsRe/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Component Variables
+    Rigidbody playerRb;
+    [SerializeField] PlayerInput input;
+
+    //Input Variables
+    Vector2 move;
+    Vector3 movement;
+    bool isMovementPressed;
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        input.actions["Move"].performed += OnMovementPressed;
+        input.actions["Move"].started += OnMovementPressed;
+        input.actions["Move"].canceled += OnMovementPressed;
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(isMovementPressed)
+        {
+            Debug.Log("Working");
+        }
+    }
+    
+    void OnMovementPressed(InputAction.CallbackContext ctx)
+    {
+        move = ctx.ReadValue<Vector2>();
+        isMovementPressed = move.x != 0 || move.y != 0;
+        movement.x = move.x;
+        movement.z = move.y;
     }
 }
